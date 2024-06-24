79c8d7e8c8c99e3be3c4f55130046c76c8a23a9a
Ilya Fadeev <109843076+weirdddlich@users.noreply.github.com>
2024-06-24 08:58:11 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Windows.Media.Colors;
+using System.Drawing;
+
+namespace Calculator;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void BtnCalculateOnClick(object sender, RoutedEventArgs e)
+    {
+        if (double.TryParse(TxtHeight.Text, out double height) &&
+            double.TryParse(TxtWeight.Text, out double weight) &&
+        double.TryParse(TxtUglevod.Text, out double uglevod))
+        {
+        
+            double imt = (height * 4) + (weight * 9) + (uglevod * 4);
+
+            TxtResult.Text = $"Всего калорий: {imt}";
+
+            string status = "";
+            string recomendations = "";
+            
+        }
+        else
+        {
+            MessageBox.Show("Пожалуйста, введите корректные значения", "Ошибка ввода");
+        }
+    }
+}
